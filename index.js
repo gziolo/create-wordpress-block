15b60e34a887f2e9e710bb43467e5a406dc1b489
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-12-07 21:38:46 +0100

    Add basic set of prompts for the most popular settings

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log( 'To be implemented...' );
+const inquirer = require( 'inquirer' );
+
+const categories = [ 'common', 'embed', 'formatting', 'layout', 'widgets' ];
+
+inquirer
+	.prompt( [
+        {
+            type: 'input',
+            name: 'slug',
+            message: 'The internal name of the block',
+        },
+        {
+            type: 'input',
+            name: 'namespace',
+            message: 'The internal namespace for the block',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'The display title for your block',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'The short description for your block',
+        },
+        {
+            type: 'input',
+            name: 'dashicon',
+            message: 'The dashicon to make it easier to identify your block'
+        },
+		{
+			type: 'list',
+			name: 'category',
+			message: 'The category name to help users browse and discover your block',
+            choices: categories,
+		},
+	] )
+	.then( ( answers ) => {
+		console.log( JSON.stringify( answers, null, '  ' ) );
+	} );
