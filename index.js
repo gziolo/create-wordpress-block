d391dc5ffe003dae3d73be068f3d606488627519
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-12-08 20:13:32 +0100

    Improve prompts to have better descriptions and defaults provided

--- index.js
+++ index.js
@@ -6,34 +6,40 @@ inquirer
 	.prompt( [
         {
             type: 'input',
-            name: 'slug',
-            message: 'The internal name of the block',
+            name: 'namespace',
+            message: 'The internal namespace for the block, the name of the plugin or theme',
+            default: 'create-wordpress-block',
         },
         {
             type: 'input',
-            name: 'namespace',
-            message: 'The internal namespace for the block',
+            name: 'slug',
+            message: 'The internal name for the block',
+            default: 'es5-demo',
         },
         {
             type: 'input',
             name: 'title',
             message: 'The display title for your block',
+            default: 'ES5 Demo',
         },
         {
             type: 'input',
             name: 'description',
-            message: 'The short description for your block',
+            message: 'The short description for your block (optional)',
+            default: 'Demo block written with ES5 standard and no JSX. No build step required.'
         },
         {
             type: 'input',
             name: 'dashicon',
-            message: 'The dashicon to make it easier to identify your block'
+            message: 'The dashicon to make it easier to identify your block (optional)',
+            default: 'smiley',
         },
 		{
 			type: 'list',
 			name: 'category',
 			message: 'The category name to help users browse and discover your block',
             choices: categories,
+            default: 'common',
 		},
 	] )
 	.then( ( answers ) => {
